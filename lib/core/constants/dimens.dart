6710e016b193d8a8f1cd47cfd0519c24e355837a
senen <senen@trialing.org>
2025-03-13 19:45:00 +0100

    Add dimens.dart for consistent padding values

---
+++ lib/core/constants/dimens.dart
@@ -0,0 +1,9 @@
+class Dimens {
+  static const paddingXXSmall = 2.0;
+  static const paddingXSmall = 4.0;
+  static const paddingSmall = 8.0;
+  static const paddingMedium = 16.0;
+  static const paddingLarge = 24.0;
+  static const paddingXLarge = 32.0;
+  static const paddingXXLarge = 48.0;
+}
